c3e4fc913da189910a36de0bdeabf5113176bf78
Ryan Duffendack <ryan.duffendack@gmail.com>
2023-03-22 13:13:36 -0500

    set up main classes structure

---
+++ src/field.rs
@@ -0,0 +1,26 @@
+pub struct farm {
+    field: vec<vec<str>>,
+    score: i32,
+    max_cows: i32,
+    num_cows: i32,
+    pub space_left: i32,
+    pub size: i32,
+}
+
+impl farm {
+    pub fn read_farm(&self, file: String) -> farm {
+
+    }
+    pub fn read_farm(&self, prev: farm) -> farm {
+
+    }
+    pub fn score_farm(&self) -> i32 {
+
+    }
+    pub fn add_cow(&mut self, loc: vec<i32>) -> bool {
+
+    }
+    pub fn remove_cow(&mut self, loc: vec<i32>) -> bool {
+
+    }
+}
---
+++ src/intel.rs
@@ -0,0 +1,11 @@
+use rand::Rng;
+
+pub struct intel {
+
+}
+
+impl intel {
+    pub fn random_move(board: farm) -> vec<i32> {
+        [rng.gen_range(0..farm.size), rng.gen_range(0..farm.size)];
+    }
+}
